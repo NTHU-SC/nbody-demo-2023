1cc9ee08969fae79b0d17b010ea000815b071665
Paulius Velesko <paulius.velesko@intel.com>
2019-05-13 16:21:56 +0000

    defualt ratio to 100%

--- ver5_sycl/main.cpp
+++ ver5_sycl/main.cpp
@@ -26,7 +26,7 @@ int main(int argc, char** argv)
 {
   int N;			//number of particles
   int nstep; 		//number ot integration steps
-  float ratio; //% of work to be done on CPU
+  float ratio=1.0f; //% of work to be done on CPU
   
   GSimulation sim;
     
@@ -41,9 +41,9 @@ int main(int argc, char** argv)
     }
     if(argc==4) 
       ratio = atof(argv[3]);
-      sim.set_workshare(ratio);
   }
   
+  sim.set_workshare(ratio);
   sim.start();
 
   return 0;
