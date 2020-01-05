b29e0acfe09daad72bf4a33594765dc0b6a709f1
Paulius Velesko <paulius.velesko@intel.com>
2020-01-05 16:03:49 +0000

    CPU code style fix

--- ver5_all/programming_models/cpu/GSimulation.cpp
+++ ver5_all/programming_models/cpu/GSimulation.cpp
@@ -134,90 +134,85 @@ void GSimulation :: start()
   int nf = 0;
   
   const double t0 = time.start();
-  for (int s=1; s<=get_nsteps(); ++s)
-  {   
-   ts0 += time.start(); 
-   for (i = 0; i < n; i++)// update acceleration
-   {
+  for (int s=1; s<=get_nsteps(); ++s) {
+    ts0 += time.start(); 
+    for (i = 0; i < n; i++) { // update acceleration
 #ifdef ASALIGN
-     __assume_aligned(particles->pos_x, alignment);
-     __assume_aligned(particles->pos_y, alignment);
-     __assume_aligned(particles->pos_z, alignment);
-     __assume_aligned(particles->acc_x, alignment);
-     __assume_aligned(particles->acc_y, alignment);
-     __assume_aligned(particles->acc_z, alignment);
-     __assume_aligned(particles->mass, alignment);
+      __assume_aligned(particles->pos_x, alignment);
+      __assume_aligned(particles->pos_y, alignment);
+      __assume_aligned(particles->pos_z, alignment);
+      __assume_aligned(particles->acc_x, alignment);
+      __assume_aligned(particles->acc_y, alignment);
+      __assume_aligned(particles->acc_z, alignment);
+      __assume_aligned(particles->mass, alignment);
 #endif
-     real_type ax_i = particles->acc_x[i];
-     real_type ay_i = particles->acc_y[i];
-     real_type az_i = particles->acc_z[i];
+      real_type ax_i = particles->acc_x[i];
+      real_type ay_i = particles->acc_y[i];
+      real_type az_i = particles->acc_z[i];
 #pragma omp simd reduction(+:ax_i,ay_i,az_i)
-     for (j = 0; j < n; j++)
-     {
-         real_type dx, dy, dz;
-	 real_type distanceSqr = 0.0f;
-	 real_type distanceInv = 0.0f;
-		  
-	 dx = particles->pos_x[j] - particles->pos_x[i];	//1flop
-	 dy = particles->pos_y[j] - particles->pos_y[i];	//1flop	
-	 dz = particles->pos_z[j] - particles->pos_z[i];	//1flop
-	
- 	 distanceSqr = dx*dx + dy*dy + dz*dz + softeningSquared;	//6flops
- 	 distanceInv = 1.0f / sqrtf(distanceSqr);			//1div+1sqrt
+      for (j = 0; j < n; j++) {
+        real_type dx, dy, dz;
+        real_type distanceSqr = 0.0f;
+        real_type distanceInv = 0.0f;
+        
+        dx = particles->pos_x[j] - particles->pos_x[i];	//1flop
+        dy = particles->pos_y[j] - particles->pos_y[i];	//1flop	
+        dz = particles->pos_z[j] - particles->pos_z[i];	//1flop
 
-	 ax_i+= dx * G * particles->mass[j] * distanceInv * distanceInv * distanceInv; //6flops
-	 ay_i += dy * G * particles->mass[j] * distanceInv * distanceInv * distanceInv; //6flops
-	 az_i += dz * G * particles->mass[j] * distanceInv * distanceInv * distanceInv; //6flops
-     }
-     particles->acc_x[i] = ax_i;
-     particles->acc_y[i] = ay_i;
-     particles->acc_z[i] = az_i;
-   }
-   energy = 0;
+        distanceSqr = dx*dx + dy*dy + dz*dz + softeningSquared;	//6flops
+        distanceInv = 1.0f / sqrtf(distanceSqr);			//1div+1sqrt
 
-   for (i = 0; i < n; ++i)// update position
-   {
-     particles->vel_x[i] += particles->acc_x[i] * dt; //2flops
-     particles->vel_y[i] += particles->acc_y[i] * dt; //2flops
-     particles->vel_z[i] += particles->acc_z[i] * dt; //2flops
-	  
-     particles->pos_x[i] += particles->vel_x[i] * dt; //2flops
-     particles->pos_y[i] += particles->vel_y[i] * dt; //2flops
-     particles->pos_z[i] += particles->vel_z[i] * dt; //2flops
+        ax_i+= dx * G * particles->mass[j] * distanceInv * distanceInv * distanceInv; //6flops
+        ay_i += dy * G * particles->mass[j] * distanceInv * distanceInv * distanceInv; //6flops
+        az_i += dz * G * particles->mass[j] * distanceInv * distanceInv * distanceInv; //6flops
+      }  
 
-     particles->acc_x[i] = 0.;
-     particles->acc_y[i] = 0.;
-     particles->acc_z[i] = 0.;
-	
-     energy += particles->mass[i] * (
-	       particles->vel_x[i]*particles->vel_x[i] + 
-               particles->vel_y[i]*particles->vel_y[i] +
-               particles->vel_z[i]*particles->vel_z[i]); //7flops
-   }
-  
-    _kenergy = 0.5 * energy; 
-    
-    ts1 += time.stop();
-    if(!(s%get_sfreq()) ) 
+      particles->acc_x[i] = ax_i;
+      particles->acc_y[i] = ay_i;
+      particles->acc_z[i] = az_i;
+    }
+
+    energy = 0;
+
+    for (i = 0; i < n; ++i)// update position
     {
+      particles->vel_x[i] += particles->acc_x[i] * dt; //2flops
+      particles->vel_y[i] += particles->acc_y[i] * dt; //2flops
+      particles->vel_z[i] += particles->acc_z[i] * dt; //2flops
+     
+      particles->pos_x[i] += particles->vel_x[i] * dt; //2flops
+      particles->pos_y[i] += particles->vel_y[i] * dt; //2flops
+      particles->pos_z[i] += particles->vel_z[i] * dt; //2flops
+
+      particles->acc_x[i] = 0.;
+      particles->acc_y[i] = 0.;
+      particles->acc_z[i] = 0.;
+
+      energy += particles->mass[i] * (
+          particles->vel_x[i]*particles->vel_x[i] + 
+                particles->vel_y[i]*particles->vel_y[i] +
+                particles->vel_z[i]*particles->vel_z[i]); //7flops
+    }
+
+    _kenergy = 0.5 * energy; 
+
+    ts1 += time.stop();
+    if(!(s%get_sfreq())) {
       nf += 1;      
       std::cout << " " 
-		<<  std::left << std::setw(8)  << s
-		<<  std::left << std::setprecision(5) << std::setw(8)  << s*get_tstep()
-		<<  std::left << std::setprecision(5) << std::setw(12) << _kenergy
-		<<  std::left << std::setprecision(5) << std::setw(12) << (ts1 - ts0)
-		<<  std::left << std::setprecision(5) << std::setw(12) << gflops*get_sfreq()/(ts1 - ts0)
-		<<  std::endl;
-      if(nf > 2) 
-      {
-	av  += gflops*get_sfreq()/(ts1 - ts0);
-	dev += gflops*get_sfreq()*gflops*get_sfreq()/((ts1-ts0)*(ts1-ts0));
+      <<  std::left << std::setw(8)  << s
+      <<  std::left << std::setprecision(5) << std::setw(8)  << s*get_tstep()
+      <<  std::left << std::setprecision(5) << std::setw(12) << _kenergy
+      <<  std::left << std::setprecision(5) << std::setw(12) << (ts1 - ts0)
+      <<  std::left << std::setprecision(5) << std::setw(12) << gflops*get_sfreq()/(ts1 - ts0)
+      <<  std::endl;
+      if(nf > 2) {
+        av  += gflops*get_sfreq()/(ts1 - ts0);
+        dev += gflops*get_sfreq()*gflops*get_sfreq()/((ts1-ts0)*(ts1-ts0));
       }
-      
       ts0 = 0;
       ts1 = 0;
-    }
-  
+      }
   } //end of the time step loop
   
   const double t1 = time.stop();
