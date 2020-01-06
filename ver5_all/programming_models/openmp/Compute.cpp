de4a8dde11039745efc87dc7e00d3ee28c333794
Paulius Velesko <paulius.velesko@intel.com>
2020-01-06 10:16:04 +0000

    openmp moved to Compute.cpp

--- ver5_all/programming_models/openmp/Compute.cpp
+++ ver5_all/programming_models/openmp/Compute.cpp
@@ -20,78 +20,6 @@
 
 #include "GSimulation.hpp"
 #include "cpu_time.hpp"
-#define sqrt
-#define sqrtf 
-GSimulation :: GSimulation()
-{
-  std::cout << "===============================" << std::endl;
-  std::cout << " Initialize Gravity Simulation" << std::endl;
-  set_npart(2000); 
-  set_nsteps(500);
-  set_tstep(0.1); 
-  set_sfreq(50);
-}
-
-void GSimulation :: set_number_of_particles(int N)  
-{
-  set_npart(N);
-}
-
-void GSimulation :: set_number_of_steps(int N)  
-{
-  set_nsteps(N);
-}
-
-void GSimulation :: init_pos()
-{
-//  std::random_device rd;        //random number generator
-//  std::mt19937 gen(42);
-//  std::uniform_real_distribution<real_type> unif_d(0,1.0);
-    srand(123);
-  for(int i=0; i<get_npart(); ++i)
-  {
-//    particles_pos_x[i] = unif_d(gen);
-//    particles_pos_y[i] = unif_d(gen);
-//    particles_pos_z[i] = unif_d(gen);
-  }
-}
-
-void GSimulation :: init_vel()
-{
-//  std::random_device rd;        //random number generator
-//  std::mt19937 gen(42);
-//  std::uniform_real_distribution<real_type> unif_d(-1.0,1.0);
-
-  for(int i=0; i<get_npart(); ++i)
-  {
-//    particles_vel_x[i] = unif_d(gen) * 1.0e-3f;
-//    particles_vel_y[i] = unif_d(gen) * 1.0e-3f;
-//    particles_vel_z[i] = unif_d(gen) * 1.0e-3f;
-  }
-}
-
-void GSimulation :: init_acc()
-{
-  for(int i=0; i<get_npart(); ++i)
-  {
-    particles_acc_x[i] = 0.f;
-    particles_acc_y[i] = 0.f;
-    particles_acc_z[i] = 0.f;
-  }
-}
-
-void GSimulation :: init_mass()
-{
-  real_type n   = static_cast<real_type> (get_npart());
-//  std::random_device rd;        //random number generator
-//  std::mt19937 gen(42);
-//  std::uniform_real_distribution<real_type> unif_d(0.0,1.0);
-
-  for(int i=0; i<get_npart(); ++i)
-  {
-//    particles_mass[i] = n * unif_d(gen);
-  }
-}
 
 void GSimulation :: start() 
 {
@@ -101,21 +29,49 @@ void GSimulation :: start()
   int i,j;
  
   const int alignment = 32;
-  particles_pos_x = (real_type*) malloc(n*sizeof(real_type));
-  particles_pos_y = (real_type*) malloc(n*sizeof(real_type));
-  particles_pos_z = (real_type*) malloc(n*sizeof(real_type));
-  particles_vel_x = (real_type*) malloc(n*sizeof(real_type));
-  particles_vel_y = (real_type*) malloc(n*sizeof(real_type));
-  particles_vel_z = (real_type*) malloc(n*sizeof(real_type));
-  particles_acc_x = (real_type*) malloc(n*sizeof(real_type));
-  particles_acc_y = (real_type*) malloc(n*sizeof(real_type));
-  particles_acc_z = (real_type*) malloc(n*sizeof(real_type));
-  particles_mass  = (real_type*) malloc(n*sizeof(real_type));
+  auto particles_pos_x = (real_type*) malloc(n*sizeof(real_type));
+  auto particles_pos_y = (real_type*) malloc(n*sizeof(real_type));
+  auto particles_pos_z = (real_type*) malloc(n*sizeof(real_type));
+  auto particles_vel_x = (real_type*) malloc(n*sizeof(real_type));
+  auto particles_vel_y = (real_type*) malloc(n*sizeof(real_type));
+  auto particles_vel_z = (real_type*) malloc(n*sizeof(real_type));
+  auto particles_acc_x = (real_type*) malloc(n*sizeof(real_type));
+  auto particles_acc_y = (real_type*) malloc(n*sizeof(real_type));
+  auto particles_acc_z = (real_type*) malloc(n*sizeof(real_type));
+  auto particles_mass  = (real_type*) malloc(n*sizeof(real_type));
+
+  particles = (ParticleSoA*) aligned_alloc(alignment, sizeof(ParticleSoA));
+  particles->pos_x = (real_type*) aligned_alloc(alignment, n*sizeof(real_type));
+  particles->pos_y = (real_type*) aligned_alloc(alignment, n*sizeof(real_type));
+  particles->pos_z = (real_type*) aligned_alloc(alignment, n*sizeof(real_type));
+  particles->vel_x = (real_type*) aligned_alloc(alignment, n*sizeof(real_type));
+  particles->vel_y = (real_type*) aligned_alloc(alignment, n*sizeof(real_type));
+  particles->vel_z = (real_type*) aligned_alloc(alignment, n*sizeof(real_type));
+  particles->acc_x = (real_type*) aligned_alloc(alignment, n*sizeof(real_type));
+  particles->acc_y = (real_type*) aligned_alloc(alignment, n*sizeof(real_type));
+  particles->acc_z = (real_type*) aligned_alloc(alignment, n*sizeof(real_type));
+  particles->mass  = (real_type*) aligned_alloc(alignment, n*sizeof(real_type));
  
   init_pos();	
   init_vel();
   init_acc();
   init_mass();
+
+  for (int i = 0; i < n; i++) {
+    particles_pos_x[i] = particles->pos_x[i];
+    particles_pos_y[i] = particles->pos_y[i];
+    particles_pos_z[i] = particles->pos_z[i];
+
+    particles_vel_x[i] = particles->vel_x[i];
+    particles_vel_y[i] = particles->vel_y[i];
+    particles_vel_z[i] = particles->vel_z[i];
+
+    particles_acc_x[i] = particles->acc_x[i];
+    particles_acc_y[i] = particles->acc_y[i];
+    particles_acc_z[i] = particles->acc_z[i];
+
+    particles_mass[i] = particles->mass[i];
+  }
   
   print_header();
   
@@ -255,41 +211,5 @@ map(to: tmp_particles_mass [:n]) \
   std::cout << "# Total Time (s)     : " << _totTime << std::endl;
   std::cout << "# Average Perfomance : " << av << " +- " <<  dev << std::endl;
   std::cout << "===============================" << std::endl;
-
-}
-
-
-void GSimulation :: print_header()
-{
-	    
-  std::cout << " nPart = " << get_npart()  << "; " 
-	    << "nSteps = " << get_nsteps() << "; " 
-	    << "dt = "     << get_tstep()  << std::endl;
-	    
-  std::cout << "------------------------------------------------" << std::endl;
-  std::cout << " " 
-	    <<  std::left << std::setw(8)  << "s"
-	    <<  std::left << std::setw(8)  << "dt"
-	    <<  std::left << std::setw(12) << "kenergy"
-	    <<  std::left << std::setw(12) << "time (s)"
-	    <<  std::left << std::setw(12) << "GFlops"
-	    <<  std::endl;
-  std::cout << "------------------------------------------------" << std::endl;
-
-
-}
-
-GSimulation :: ~GSimulation()
-{
-  free(particles_pos_x);
-  free(particles_pos_y);
-  free(particles_pos_z);
-  free(particles_vel_x);
-  free(particles_vel_y);
-  free(particles_vel_z);
-  free(particles_acc_x);
-  free(particles_acc_y);
-  free(particles_acc_z);
-  free(particles_mass);
 
 }
