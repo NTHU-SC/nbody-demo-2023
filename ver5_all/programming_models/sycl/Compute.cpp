3d96994ad28336aa525d4bac55ed48223517f89a
Paulius Velesko <paulius.velesko@intel.com>
2020-01-06 08:55:16 +0000

    moving sycl to compute.cpp

--- ver5_all/programming_models/sycl/Compute.cpp
+++ ver5_all/programming_models/sycl/Compute.cpp
@@ -25,77 +25,6 @@
 
 using namespace cl::sycl;
 
-GSimulation :: GSimulation()
-{
-  std::cout << "===============================" << std::endl;
-  std::cout << " Initialize Gravity Simulation" << std::endl;
-  set_npart(2000); 
-  set_nsteps(500);
-  set_tstep(0.1); 
-  set_sfreq(50);
-}
-
-void GSimulation :: set_number_of_particles(int N)  
-{
-  set_npart(N);
-}
-
-void GSimulation :: set_number_of_steps(int N)  
-{
-  set_nsteps(N);
-}
-
-void GSimulation :: init_pos()
-{
-  std::random_device rd;        //random number generator
-  std::mt19937 gen(42);
-  std::uniform_real_distribution<real_type> unif_d(0,1.0);
-
-  for(int i=0; i<get_npart(); ++i)
-  {
-    particles->pos_x[i] = unif_d(gen);
-    particles->pos_y[i] = unif_d(gen);
-    particles->pos_z[i] = unif_d(gen);
-  }
-}
-
-void GSimulation :: init_vel()
-{
-  std::random_device rd;        //random number generator
-  std::mt19937 gen(42);
-  std::uniform_real_distribution<real_type> unif_d(-1.0,1.0);
-
-  for(int i=0; i<get_npart(); ++i)
-  {
-    particles->vel_x[i] = unif_d(gen) * 1.0e-3f;
-    particles->vel_y[i] = unif_d(gen) * 1.0e-3f;
-    particles->vel_z[i] = unif_d(gen) * 1.0e-3f;
-  }
-}
-
-void GSimulation :: init_acc()
-{
-  for(int i=0; i<get_npart(); ++i)
-  {
-    particles->acc_x[i] = 0.f;
-    particles->acc_y[i] = 0.f;
-    particles->acc_z[i] = 0.f;
-  }
-}
-
-void GSimulation :: init_mass()
-{
-  real_type n   = static_cast<real_type> (get_npart());
-  std::random_device rd;        //random number generator
-  std::mt19937 gen(42);
-  std::uniform_real_distribution<real_type> unif_d(0.0,1.0);
-
-  for(int i=0; i<get_npart(); ++i)
-  {
-    particles->mass[i] = n * unif_d(gen);
-  }
-}
-
 void GSimulation :: start() 
 {
   queue q = queue(gpu_selector{});
@@ -277,39 +206,4 @@ void GSimulation :: start()
   std::cout << "# Average Perfomance : " << av << " +- " <<  dev << std::endl;
   std::cout << "===============================" << std::endl;
 
-}
-
-void GSimulation :: print_header()
-{
-	    
-  std::cout << " nPart = " << get_npart()  << "; " 
-	    << "nSteps = " << get_nsteps() << "; " 
-	    << "dt = "     << get_tstep()  << std::endl;
-	    
-  std::cout << "------------------------------------------------" << std::endl;
-  std::cout << " " 
-	    <<  std::left << std::setw(8)  << "s"
-	    <<  std::left << std::setw(8)  << "dt"
-	    <<  std::left << std::setw(12) << "kenergy"
-	    <<  std::left << std::setw(12) << "time (s)"
-	    <<  std::left << std::setw(12) << "GFlops"
-	    <<  std::endl;
-  std::cout << "------------------------------------------------" << std::endl;
-
-
-}
-
-GSimulation :: ~GSimulation()
-{
-  free(particles->pos_x);
-  free(particles->pos_y);
-  free(particles->pos_z);
-  free(particles->vel_x);
-  free(particles->vel_y);
-  free(particles->vel_z);
-  free(particles->acc_x);
-  free(particles->acc_y);
-  free(particles->acc_z);
-  free(particles->mass);
-  free(particles);
 }
